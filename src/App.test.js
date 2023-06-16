77b02122667b1f3048aa87bb3cad25fe5358ea9b
gnostertag <ostertag2000@gmail.com>
2023-06-16 12:13:37 -0700

    cleaned

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders tic tac toe header', () => {
-  render(<App/>);
-  const header = screen.getByText('Tic Tac Toe'); 
+test("renders tic tac toe header", () => {
+  render(<App />);
+  const header = screen.getByText("Tic Tac Toe");
 });
